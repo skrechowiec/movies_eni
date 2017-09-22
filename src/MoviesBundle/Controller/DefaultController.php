c663154b0625fd61b8559a9010809240a31c282f
= <=>
2017-09-22 15:05:13 +0200

    first commit

---
+++ src/MoviesBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace MoviesBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('MoviesBundle:Default:index.html.twig');
+    }
+}
